5c8757bdf5c39d0cebdecc44412dad6eac96bec0
Michael Morel <Aegis@Michaels-MacBook-Pro-2.local>
2017-01-04 02:47:37 -0500

    Fixed methods. Files properly JSON formatted. Add and remove features work with JSON in mind. Input compares against values within each dictionary. Will work on GUI next

--- Register.py
+++ Register.py
@@ -1,70 +1,75 @@
+import requests
+import json
+
 def add_menu_item(menu, name, price):
-	menu[name] = price
+	new_entry = {}
+	new_entry["name"] = name
+	new_entry["price"] = price
+	menu.append(new_entry)
+	print (menu)
+	master_menu = open("menu.json", 'w')
+	master_menu = json.dump(menu, master_menu)
+	return master_menu
 
 def rem_menu_item(menu, name):
-	if menu.get(name, "Doesn't exist"):
-		del menu[name]
+	for item in menu:
+		if item['name'] == name:
+			menu.remove(item)
+			master_menu = open("menu.json", 'w')
+		master_menu = json.dump(menu, master_menu)
 
 def order_input(menu):
-	orders = {}
+	orders = []
 	x = 1
 	total = 0
+	found = False
 	while (True):
 		if x < 2:
 			order = input('What would you like to order? (Enter Q to finish your order)\n')
 		else:
 			order = input("Anything else for you today? (Enter Q to finish your order)\n")
-		found = menu.get(order)
-		if order.lower() == 'bazinga':
-			print ("Sorry, all out of the Bazinga.")
-			continue
+		for item in menu:
+			if order == item['name']:
+				total += item['price']
+				orders.append(item['name'])
+				found = item
+				print(orders)
+		if found is False:
+				print ("Menu item doesn't exist")
 		if order.upper() == 'Q':
-			store_order(orders)
+			store_order(orders, total)
 			print(total)
 			break
-		if found:
-			total += menu[order]
-			orders[order] = menu[order]
-			print(orders)
-		else:
-			print ("Menu item doesn't exist")
 		x += 1
 
-def store_order(order):
+def store_order(order, total):
 	sales_log = open("Today's orders.txt", 'a')
-	total = 0
-	for item, price in order.items():
-		total += price
-		sales_log.write (item + ' ' + format(price, '.2f') + "\n")
+	for name in order:
+		sales_log.write (name + "\n")
 	sales_log.write ("total = " + format(total, '.2f') +  '\n\n')
 	sales_log.close
 
 def load_menu(menu):
-	file = open("menu.txt", 'r')
-	name = []
-	price = []
-	for line in file:
-		line = line.strip()
-		if type(line) == str:
-			name.append(line)
-		elif type(line) == int:
-			price.append(line)
-		else:
-			raise TypeError
-	for item, value in zip(name, price):
-		menu[item] = price
-	print(menu)
-	return menu
+	try:
+		menu_request = requests.get("http://link to website here")
+		daily_menu = menu_request.json()
+	except:
+		print("Could not connect with website. Loading Backup Menu.")
+		try:
+			daily_menu = open("menu.json", 'r')
+		except:
+			print("File does not exist.")
+	return daily_menu
 
 
 def main():
-	menu = {}
+	menu = []
 	load_menu(menu)
 	add_menu_item(menu, 'Hamburger', 1.99)
 	print(menu)
 	add_menu_item(menu, 'Double Cheeseburger', 2.99)
 	print(menu)
-	rem_menu_item(menu, 'Hamburger')
+	#rem_menu_item(menu, 'Hamburger')
 	print(menu)
 	order_input(menu)
 main()
